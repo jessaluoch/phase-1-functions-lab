114445463dd266ffa45542a93071830492e3cef6
jessaluoch <jessica.aluoch@student.moringaschool.com>
2023-10-26 13:08:16 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42; // Scuber's headquarters location
+    return Math.abs(pickupLocation - hqLocation);
+  }
+
+  function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    return blocks * 264;
+  }
+
+  function distanceTravelledInFeet(startBlock, destinationBlock) {
+    return Math.abs(destinationBlock - startBlock) * 264;
+  }
+
+  function calculatesFarePrice(startBlock, endBlock) {
+    const distanceInFeet = distanceTravelledInFeet(startBlock, endBlock);
+  
+    if (distanceInFeet <= 400) {
+      return 0; // First 400 feet are free.
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+      return 25; // Flat fare for distances between 2000 and 2500 feet.
+    } else {
+      return 'cannot travel that far';
+    }
+  }
